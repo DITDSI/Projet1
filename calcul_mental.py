3e80d59e8b98418c5db02f5dbf02a59f72ee7160
Moussa Camara <moussacamara@MacBook-Air-de-Moussa.local>
2021-09-07 16:14:43 +0000

    Apprenez à compter

---
+++ calcul_mental.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  7 16:07:18 2021
+
+@author: moussacamara
+"""
+
+x=5
+y=10
+if x==y:
+    print("Ce n'est pas vrai!")
+else:
+    print("Il faut apprendre à compter!")
